157ee9f4bb9d23312c97b0a0965cfc081e5283fd
Soumyadeep Das <soumyadeep.dbsslg@gmail.com>
2022-01-15 14:17:02 +0530

    Autocompletion updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -20,24 +20,57 @@ class _HomeScreenState extends State<HomeScreen> {
             )
           : ListView(
               children: [
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Search Location here',
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search Location here',
+                      suffixIcon: Icon(Icons.search),
+                    ),
+                    onChanged: (value) => applicationBloc.searchPlaces(value),
                   ),
                 ),
-                Container(
-                  height: 300.0,
-                  child: GoogleMap(
-                    mapType: MapType.normal,
-                    myLocationEnabled: true,
-                    initialCameraPosition: CameraPosition(
-                      target: LatLng(
-                        applicationBloc.currentLocation.latitude,
-                        applicationBloc.currentLocation.longitude,
+                Stack(
+                  children: [
+                    Container(
+                      height: 300.0,
+                      child: GoogleMap(
+                        mapType: MapType.normal,
+                        myLocationEnabled: true,
+                        initialCameraPosition: CameraPosition(
+                          target: LatLng(
+                            applicationBloc.currentLocation.latitude,
+                            applicationBloc.currentLocation.longitude,
+                          ),
+                          zoom: 14.0,
+                        ),
                       ),
-                      zoom: 14.0,
                     ),
-                  ),
+                    if (applicationBloc.searchResults != null &&
+                        applicationBloc.searchResults.length != 0)
+                      Container(
+                        height: 300.0,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(0.6),
+                          backgroundBlendMode: BlendMode.darken,
+                        ),
+                      ),
+                    Container(
+                      height: 300.0,
+                      child: ListView.builder(
+                        itemCount: applicationBloc.searchResults.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(
+                              applicationBloc.searchResults[index].description,
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
